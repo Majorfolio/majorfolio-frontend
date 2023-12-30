41ec32c3db9903969518ff627688dfc464e9ae59
cyLee1111 <flora021111@gmail.com>
2023-12-30 23:53:54 +0900

    fix: change css-modules to styled-components

--- majorfolio/src/pages/Landing/Landing.jsx
+++ majorfolio/src/pages/Landing/Landing.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import styles from "./Landing.module.css";
+import * as S from "./Landing.styles";
 
-import { Mf1, Mf2, Mf3, Mf4, LandingLogo, LandingCurve, MfIcon1, MfIcon2, MfIcon3, MfIcon4, DottedLine } from '../../assets';
 import dollarIcon from '../../assets/landing-dollar.png';
 import clipBoardIcon from '../../assets/landing-clipboard.png';
 import calendarIcon from '../../assets/landing-calendar.png';
@@ -11,149 +10,149 @@ import pencilIcon from '../../assets/landing-pencil.png';
 const Landing = () => {
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.mfWrapper} >
-          <Mf1 className={styles.mf1} />
-          <Mf2 className={styles.mf2} />
-        </div>
+      <S.Container>
+        <S.MfWrapper>
+          <S.MfImg1 />
+          <S.MfImg2 />
+        </S.MfWrapper>
 
-        <div className={styles.logoWrapper}>
-          <LandingLogo className={styles.logo} />
-        </div>
+        <S.LogoWrapper>
+          <S.Logo />
+        </S.LogoWrapper>
 
-        <p className={styles.text1} >지난 학기 잘 만든 과제를 볼 수 있다는<br/>사실을 알고 계셨나요?</p>
-        <div className={styles.speechBubbleWrapper}>
-          <p className={`${styles.text2} ${styles.centerAlign}`} >선배들의 과제나 자료,<br/>보고 싶지 않으신가요?</p>
-          <span className={`${styles.speechBubble} ${styles.b1}`}>
+        <S.Text1>지난 학기 잘 만든 과제를 볼 수 있다는<br/>사실을 알고 계셨나요?</S.Text1>
+        <S.SpeechBubbleWrapper>
+          <S.CenterAlign>선배들의 과제나 자료,<br/>보고 싶지 않으신가요?</S.CenterAlign>
+          <S.B1>
             선배한테 직접 물어보기는 무서워..
-          </span>
-          <span className={`${styles.speechBubble} ${styles.opacity} ${styles.b2}`}>
+          </S.B1>
+          <S.B2>
             잘하고 싶은데 방법을 모르겠어 ㅠㅠ
-          </span>
-          <span className={`${styles.speechBubble} ${styles.opacity} ${styles.b3}`}>
+          </S.B2>
+          <S.B3>
             어떡하지
-          </span>
-          <span className={`${styles.speechBubble} ${styles.opacity} ${styles.b4}`}>
+          </S.B3>
+          <S.B4>
             어려워..
-          </span>
-          <span className={`${styles.speechBubble} ${styles.opacity} ${styles.b5}`}>
+          </S.B4>
+          <S.B5>
             이 툴은 어떻게 쓰는거지?
-          </span>
-          <span className={`${styles.speechBubble} ${styles.b6}`}>
+          </S.B5>
+          <S.B6>
             작년 이 수업에서 A+ 받은 과제가 뭘까?
-          </span>
-        </div>
+          </S.B6>
+        </S.SpeechBubbleWrapper>
 
-        <div className={styles.curveWrapper} >
-          <p className={`${styles.text2} ${styles.whiteText} ${styles.centerAlign}`} >선배들의 자료들을 커피 한 잔 값으로<br/>사고 성장할 수 있습니다!</p>
-          <LandingCurve className={styles.curve} />
-        </div>
+        <S.CurveWrapper>
+          <S.WhiteText>선배들의 자료들을 커피 한 잔 값으로<br/>사고 성장할 수 있습니다!</S.WhiteText>
+          <S.Curve />
+        </S.CurveWrapper>
 
-        <div className={styles.seniorWrapper} >
-          <div className={styles.seniorTextWrapper} >
+        <S.SeniorWrapper>
+          <S.SeniorTextWrapper>
             <div>
-              <p className={`${styles.text2} ${styles.blueText}`} >선배들은</p>
-              <p className={styles.text2} >자신의 과제로 돈을 벌어요</p>
+              <S.BlueText>선배들은</S.BlueText>
+              <S.Text2>자신의 과제로 돈을 벌어요</S.Text2>
             </div>
-            <p className={styles.text3} >그동안 만들었던 과제, 발표자료로<br/>커피 한 잔 값 벌어봐요!</p>
-          </div>
+            <S.Text3>그동안 만들었던 과제, 발표자료로<br/>커피 한 잔 값 벌어봐요!</S.Text3>
+          </S.SeniorTextWrapper>
           
-          <Mf3 className={styles.mf3} />
-          <span className={`${styles.iconWrapper} ${styles.ic1}`} ><img src={clipBoardIcon} alt="clip" width={35} height={35} /></span>
-          <span className={`${styles.iconWrapper} ${styles.ic2}`} ><img src={clipBoardIcon} alt="clip" width={35} height={35} /></span>
-          <span className={`${styles.iconWrapper} ${styles.ic3}`} ><img src={clipBoardIcon} alt="clip" width={35} height={35} /></span>
-          <span className={`${styles.iconWrapper} ${styles.ic4}`} ><img src={clipBoardIcon} alt="clip" width={35} height={35} /></span>
-          <span className={`${styles.iconWrapper} ${styles.ic5}`} ><img src={clipBoardIcon} alt="clip" width={35} height={35} /></span>
-          <span className={`${styles.iconWrapper} ${styles.ic6}`} ><img src={dollarIcon} alt="dollar" width={35} height={35} /></span>
-        </div>
+          <S.MfImg3 />
+          <S.Ic1><S.Img src={clipBoardIcon} alt="clip" width={35} height={35} /></S.Ic1>
+          <S.Ic2><S.Img src={clipBoardIcon} alt="clip" width={35} height={35} /></S.Ic2>
+          <S.Ic3><S.Img src={clipBoardIcon} alt="clip" width={35} height={35} /></S.Ic3>
+          <S.Ic4><S.Img src={clipBoardIcon} alt="clip" width={35} height={35} /></S.Ic4>
+          <S.Ic5><S.Img src={clipBoardIcon} alt="clip" width={35} height={35} /></S.Ic5>
+          <S.Ic6><S.Img src={dollarIcon} alt="dollar" width={35} height={35} /></S.Ic6>
+        </S.SeniorWrapper>
 
-        <div className={styles.juniorWrapper} >
-          <div className={styles.juniorTextWrapper}>
+        <S.JuniorWrapper>
+          <S.JuniorTextWrapper>
             <div>
-              <p className={`${styles.text2} ${styles.orangeText}`} >후배들은</p>
-              <p className={styles.text2} >자료를 살 수 있어요</p>
+              <S.OrangeText>후배들은</S.OrangeText>
+              <S.Text2>자료를 살 수 있어요</S.Text2>
             </div>
-            <p className={styles.text3} >비결이 녹아든 자료를 공유받고<br/>소중한 내 시간과 학점을 지켜요!</p>
-          </div>
+            <S.Text3>비결이 녹아든 자료를 공유받고<br/>소중한 내 시간과 학점을 지켜요!</S.Text3>
+          </S.JuniorTextWrapper>
           
-          <Mf4 className={styles.mf4} />
-          <div className={styles.juniorDescWrapper} >
+          <S.MfImg4 />
+          <S.JuniorDescWrapper>
             <div>
-              <span className={styles.iconWrapper2} ><img src={clipBoardIcon} alt="clip" width={35} height={35} /></span>
-              <p className={styles.text4} >적은 돈으로 살 수 있는<br/>검증된 선배의 자료</p>
+              <S.IconWrapper2><S.Img src={clipBoardIcon} alt="clip" width={35} height={35} /></S.IconWrapper2>
+              <S.Text4>합리적으로 살 수 있는<br/>검증된 선배의 자료</S.Text4>
             </div>
             <div>
-              <span className={styles.iconWrapper2} ><img src={calendarIcon} alt="calendar" width={35} height={35} /></span>
-              <p className={styles.text4} >시간이 돈이다!<br/>눈에 띄게 빨라진 작업</p>
+              <S.IconWrapper2><S.Img src={calendarIcon} alt="calendar" width={35} height={35} /></S.IconWrapper2>
+              <S.Text4>시간이 돈이다!<br/>눈에 띄게 빨라진 작업</S.Text4>
             </div>
             <div>
-              <span className={styles.iconWrapper2} ><img src={pencilIcon} alt="pencil" width={35} height={35} /></span>          
-              <p className={styles.text4} >퀄리티가 한층 더<br/>높아지는 나의 과제물</p>  
+              <S.IconWrapper2><S.Img src={pencilIcon} alt="pencil" width={35} height={35} /></S.IconWrapper2>          
+              <S.Text4>퀄리티가 한층 더<br/>높아지는 나의 과제물</S.Text4>  
             </div>
-          </div>
-        </div>
+          </S.JuniorDescWrapper>
+        </S.JuniorWrapper>
 
-        <div className={styles.roleWrapper} >
-          <div className={styles.whoAreYouText} >
-            <p className={styles.text2} >여러분은<br/>어떤 사람인가요?</p>
-            <p className={styles.text3} >시간과 학점을 지키고 싶으신 분은<br/>꼭 설문에 참여해주세요!</p>
-          </div>
-          <div className={styles.mfIconWrapper} >
+        <S.RoleWrapper>
+          <S.WhoAreYouText>
+            <S.Text2>여러분은<br/>어떤 사람인가요?</S.Text2>
+            <S.Text3>시간과 학점을 지키고 싶으신 분은<br/>꼭 설문에 참여해주세요!</S.Text3>
+          </S.WhoAreYouText>
+          <S.MfIconWrapper>
             <div>
-              <MfIcon1 className={styles.mfIcon} />
-              <div className={styles.juniorName} >후배</div>
+              <S.MfCircleIcon1 />
+              <S.JuniorName >후배</S.JuniorName>
             </div>
-            <DottedLine className={styles.dottedLine} />
+            <S.DotLine />
             <div>
-              <MfIcon2 className={styles.mfIcon} />
-              <div className={styles.seniorName} >선배</div>
+              <S.MfCircleIcon2 />
+              <S.SeniorName >선배</S.SeniorName>
             </div>
-          </div>
+          </S.MfIconWrapper>
 
-          <div className={styles.roleDescWrapper} >
-            <div className={styles.roleDesc} >
-              <p>과제 말고도 할게 많은</p>
-              <p>멀티 태스커</p>
-            </div>
-            <div className={styles.roleDesc} >
-              <p>이 수업은 내가 잘 알지</p>
-              <p>교수님 취향 저격수</p>
-            </div>
-          </div>
+          <S.RoleDescWrapper >
+            <S.RoleDesc>
+              <S.P>과제 말고도 할게 많은</S.P>
+              <S.P>멀티 태스커</S.P>
+            </S.RoleDesc>
+            <S.RoleDesc>
+              <S.P>이 수업은 내가 잘 알지</S.P>
+              <S.P>교수님 취향 저격수</S.P>
+            </S.RoleDesc>
+          </S.RoleDescWrapper>
 
-          <div className={styles.mfIconWrapper} >
+          <S.MfIconWrapper>
             <div>
-              <MfIcon3 className={styles.mfIcon} />
-              <div className={styles.juniorName} >후배</div>
+              <S.MfCircleIcon3 />
+              <S.JuniorName>후배</S.JuniorName>
             </div>
-            <DottedLine className={styles.dottedLine} />
+            <S.DotLine />
             <div>
-              <MfIcon4 className={styles.mfIcon} />
-              <div className={styles.seniorName} >선배</div>
+              <S.MfCircleIcon4 />
+              <S.SeniorName >선배</S.SeniorName>
             </div>
-          </div>
+          </S.MfIconWrapper>
 
-          <div className={styles.roleDescWrapper} >
-            <div className={styles.roleDesc} >
-              <p>이 수업 A+받아야하는</p>
-              <p>학점 사냥꾼</p>
-            </div>
-            <div className={styles.roleDesc} >
-              <p>잘 만든 내 발표자료가 있는</p>
-              <p>손재주 장인</p>
-            </div>
-          </div>
-        </div>
+          <S.RoleDescWrapper >
+            <S.RoleDesc>
+              <S.P>이 수업 A+받아야하는</S.P>
+              <S.P>학점 사냥꾼</S.P>
+            </S.RoleDesc>
+            <S.RoleDesc>
+              <S.P>잘 만든 내 발표자료가 있는</S.P>
+              <S.P>손재주 장인</S.P>
+            </S.RoleDesc>
+          </S.RoleDescWrapper>
+        </S.RoleWrapper>
 
-        <div className={styles.surveyWrapper}>
-          <p className={styles.surveyTitle} >지금 바로 설문에 참여하여<br/>기프티콘 이벤트에 응모하세요!</p>
-          <div className={styles.surveyContext}>
-            <p>* Majorfolio는 사용자 동의 없이 반복적인 알림을 보내지 않습니다.</p>
-            <p>* 입력하신 개인정보는 서비스 개선 및 이용 안내 목적 이외에는<br/>사용하지 않고, 설문 종료 후 3개월 내로 폐기합니다.</p>
-          </div>
-        </div>
+        <S.SurveyWrapper>
+          <S.SurveyTitle>지금 바로 설문에 참여하여<br/>기프티콘 이벤트에 응모하세요!</S.SurveyTitle>
+          <S.SurveyContext>
+            <S.P>* Majorfolio는 사용자 동의 없이 반복적인 알림을 보내지 않습니다.</S.P>
+            <S.P>* 입력하신 개인정보는 서비스 개선 및 이용 안내 목적 이외에는<br/>사용하지 않고, 설문 종료 후 3개월 내로 폐기합니다.</S.P>
+          </S.SurveyContext>
+        </S.SurveyWrapper>
         
-      </div>
+      </S.Container>
     </>
   )
 }
--- majorfolio/src/pages/Landing/Landing.styles.jsx
+++ majorfolio/src/pages/Landing/Landing.styles.jsx
@@ -1,13 +1,16 @@
-.container {
+import styled from "styled-components";
+import { Mf1, Mf2, Mf3, Mf4, LandingLogo, LandingCurve, MfIcon1, MfIcon2, MfIcon3, MfIcon4, DottedLine } from '../../assets';
+
+export const Container = styled.div`
   width: 100vw;
-}
+`;
 
-/* 텍스트 설정 */
-p {
+// 텍스트 설정
+export const P = styled.p`
   margin: 0;
-}
+`;
 
-.text1 {
+export const Text1 = styled.p`
   margin: 0;
   color: black;
   font-size: 4.4vw;
@@ -15,26 +18,26 @@ p {
   line-height: 7.1vw;
   word-wrap: break-word;
   text-align: center;
-}
+`;
 
-.text2 {
+export const Text2 = styled.p`
   margin: 0;
   font-size: 4.4vw;
   font-weight: 700;
   line-height: 7.1vw;
   word-wrap: break-word;
-}
+`;
 
-.text3 {
+export const Text3 = styled.p` 
   margin: 0;
   color: #1F2026;
   font-size: 3.33vw;
   font-weight: 500;
   line-height: 5.33vw;
   word-wrap: break-word;
-}
+`;
 
-.text4 {
+export const Text4 = styled.p`
   margin: 1.67vw 0 0 0;
   color: #1F2026;
   font-size: 2.22vw;
@@ -42,65 +45,65 @@ p {
   line-height: 3.56vw;
   word-wrap: break-word;
   text-align: center;
-}
+`;
 
-.centerAlign {
+export const CenterAlign = styled(Text2)`
   position: absolute;
   width: 100vw;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   text-align: center;
-}
+`;
 
-.whiteText {
+export const WhiteText = styled(CenterAlign)`
   color: white;
-}
+`;
 
-.blueText {
+export const BlueText = styled(Text2)`
   color: #4340DB;
-}
+`;
 
-.orangeText {
+export const OrangeText = styled(Text2)`
   color: #FFA722;
-}
+`;
 
-/* 메인 화면 */
-.mfWrapper {
+// 메인 화면
+export const MfWrapper = styled.div`
   display: flex;
   gap: 37.5vw;
   justify-content: center;
-}
+`;
 
-.mf1 {
+export const MfImg1 = styled(Mf1)`
   width: 24vw;
   height: auto;
-}
+`;
 
-.mf2 {
+export const MfImg2 = styled(Mf2)`
   width: 20vw;
   height: auto;
-}
+`;
 
-.logoWrapper {
+export const LogoWrapper = styled.div`
   display: flex;
   margin: 5.5vw 0 8.3vw 0;
   justify-content: center;  
-}
+`;
 
-.logo {
+export const Logo = styled(LandingLogo)`
   width: 42.7vw;
   height: auto;
-}
+`;
 
-/* 말풍선 화면 (선배들의 과제나 자료 ...) */
-.speechBubbleWrapper {
+// 말풍선 화면 (선배들의 과제나 자료 ...)
+export const SpeechBubbleWrapper = styled.div`
   margin-top: 40vw;
   margin-bottom: 9.7vw;  
   position: relative;
-}
+`;
 
-.speechBubble {
+export const SpeechBubble = styled.span`
   display:inline-block;
   height: 4.4vw;
   padding: 2.2vw 4.4vw;
@@ -113,79 +116,79 @@ p {
   font-weight: 500; 
   line-height: 4.4vw; 
   word-wrap: break-word;
-}
+`;
 
-.opacity {
+export const Opacity = styled(SpeechBubble)`
   opacity: 0.20;
-}
+`;
 
-.b1 {
+export const B1 = styled(SpeechBubble)`
   margin-left: 45vw;
-}
+`;
 
-.b2 {
+export const B2 = styled(Opacity)`
   margin-top: -2.2vw;
   margin-left: 13.3vw;
-}
+`;
 
-.b3 {
+export const B3 = styled(Opacity)`
   margin-top: 1.9vw;
   margin-left: 63.6vw;
-}
+`;
 
-.b4 {
+export const B4 = styled(Opacity)`
   margin-top: 5.5vw;
   margin-left: 15.5vw;
-}
+`;
 
-.b5 {
+export const B5 = styled(Opacity)`
   margin-top: 1.9vw;
   margin-left: 50.27vw;
-}
+`;
 
-.b6 {
+export const B6 = styled(SpeechBubble)`
   margin-top: -2.2vw;
   margin-left: 8.88vw;
-}
+`;
 
-/* 곡선 배너 (선배들의 자료들을 커피 한 잔 ...) */
-.curveWrapper {
+// 곡선 배너 (선배들의 자료들을 커피 한 잔 ...)
+export const CurveWrapper = styled.div`
   width: 100vw;
   height: 30.17vw;
   position: relative;
   background: #4340DB;
-}
+`;
 
-.curve {
+export const Curve = styled(LandingCurve)`
   width: 96.39vw;
   height: auto;
   margin-left: 3.62vw;
-}
+`;
 
-/* 선배 화면 (선배들은 ...) */
-.seniorWrapper {
+// 선배 화면 (선배들은 ...)
+export const SeniorWrapper = styled.div`
   position: relative;
   height: 83.33vw;
-}
+`;
 
-.seniorTextWrapper {
+export const SeniorTextWrapper = styled.div`
   width: 50vw;
   padding-top: 8.88vw;  
   padding-left: 8.88vw;
   display: flex;
   flex-direction: column;
   gap: 2.22vw;
-}
+`;
 
-.mf3 {
+export const MfImg3 = styled(Mf3)`
   width: 18.06vw;
   height: auto;
   position: absolute;
   top: 0;
   right: 7.22vw;
-}
+`;
 
-.iconWrapper {
+export const IconWrapper = styled.span`
   width: 16.67vw;
   height: 16.67vw;
   position: absolute;
@@ -194,73 +197,73 @@ p {
   align-items: center;
   background: #F4F7FA;
   border-radius: 10vw;
-}
+`;
 
-.ic1 {
+export const Ic1 = styled(IconWrapper)`
   top: 52.27vw;
   left: 0.88vw;
   transform: rotate(13.45deg);
-}
+`;
 
-.ic2 {
+export const Ic2 = styled(IconWrapper)`
   top: 56.61vw;
   left: 18.44vw;
   transform: rotate(47.45deg);
-}
+`;
 
-.ic3 {
+export const Ic3 = styled(IconWrapper)`
   top: 65.71vw;
   left: -6.41vw;
   transform: rotate(-12.85deg);
-}
+`;
 
-.ic4 {
+export const Ic4 = styled(IconWrapper)`
   top: 65.71vw;
   left: 8.89vw;
   transform: rotate(-40.14deg);
-}
+`;
 
-.ic5 {
+export const Ic5 = styled(IconWrapper)`
   top: 65.71vw;
   left: 28.10vw;
   transform: rotate(128.92deg);
-}
+`;
 
-.ic6 {
+export const Ic6 = styled(IconWrapper)`
   top: 65.71vw;
   left: 55.56vw;
-}
+`;
 
-img {
+export const Img = styled.img`
   width: 9.72vw;
   height: auto;
-}
+`;
 
 /* 후배 화면 (후배들은 ...) */
-.juniorWrapper {
+export const JuniorWrapper = styled.div`
   position: relative;
   background: #F4F7FA;
   height: 83.33vw;
-}
+`;
 
-.juniorTextWrapper {
+export const JuniorTextWrapper = styled.div`
   padding-top: 8.88vw;  
   padding-right: 8.88vw;
   display: flex;
   flex-direction: column;
   gap: 2.22vw;
   text-align: right;
-}
+`;
 
-.mf4 {
+export const MfImg4 = styled(Mf4)`
   width: 19.22vw;
   height: auto;
   position: absolute;
   top: 13.89vw;
   left: 12.78vw;
-}
+`;
 
-.iconWrapper2 {
+export const IconWrapper2 = styled.span`
   width: 16.67vw;
   height: 16.67vw;
   margin: 0 auto;
@@ -269,49 +272,64 @@ img {
   align-items: center;
   background: #E5ECF4;
   border-radius: 10vw;
-}
+`;
 
-.juniorDescWrapper {
+export const JuniorDescWrapper = styled.div`
   width: 100vw;
   margin-top: 11.67vw;
   display: flex;
   justify-content: center;
   gap: 8.89vw;
-}
+`;
 
 /* 사용자 유형 화면 (여러분은 어떤 사람인가요?) */
-.roleWrapper {
+export const RoleWrapper = styled.div`
   height: 135.83vw;
-}
+`;
 
-.whoAreYouText {
+export const WhoAreYouText = styled.div`
   width: 100vw;
   padding: 11.11vw 0 11.9vw 0;
   display: flex;
   gap: 2.22vw;
   flex-direction: column;
   text-align: center;
-}
+`;
 
-.mfIconWrapper {
+export const MfIconWrapper = styled.div`
   margin-bottom: 2.22vw;
   display: flex;
   gap: 11.11vw;
   align-items: center;
   justify-content: center;
-}
+`;
 
-.mfIcon {
+export const MfCircleIcon1 = styled(MfIcon1)`
   width: 16.17vw;
   height: auto;
-}
+`;
 
-.dottedLine {
+export const MfCircleIcon2 = styled(MfIcon2)`
+  width: 16.17vw;
+  height: auto;
+`;
+
+export const MfCircleIcon3 = styled(MfIcon3)`
+  width: 16.17vw;
+  height: auto;
+`;
+
+export const MfCircleIcon4 = styled(MfIcon4)`
+  width: 16.17vw;
+  height: auto;
+`;
+
+export const DotLine = styled(DottedLine)`
   width: 11.11vw;
   height: auto;
-}
+`;
 
-.juniorName {
+export const JuniorName = styled.div`
   width: 3.89vw;
   height: 3.33vw;
   margin: -2.22vw auto 0 auto;
@@ -325,9 +343,9 @@ img {
   font-weight: 700;
   line-height: 3.56vw;
   word-wrap: break-word;
-}
+`;
 
-.seniorName {
+export const SeniorName = styled.div`
   width: 3.89vw;
   height: 3.33vw;
   margin: -2.22vw auto 0 auto;
@@ -341,15 +359,15 @@ img {
   font-weight: 700;
   line-height: 3.56vw;
   word-wrap: break-word;
-}
+`;
 
-.roleDescWrapper {
+export const RoleDescWrapper = styled.div`
   display: flex;
   justify-content: space-around;
   margin-bottom: 5.56vw;
-}
+`;
 
-.roleDesc {
+export const RoleDesc = styled.div`
   width: 23.33vw;
   height: 6.11vw;
   padding: 2.22vw 4.44vw;
@@ -362,10 +380,10 @@ img {
   font-weight: 500; 
   line-height: 3.11vw; 
   word-wrap: break-word;
-}
+`;
 
-/* 설문참여 화면 (지금 바로 설문에 참여하여 ...) */
-.surveyWrapper {
+// 설문참여 화면 (지금 바로 설문에 참여하여 ...)
+export const SurveyWrapper = styled.div`
   width: 100vw;
   height: 50.83vw;
   padding: auto 0;
@@ -375,17 +393,17 @@ img {
   justify-content: center;
   background: #1F2026;
   text-align: center;
-}
+`;
 
-.surveyTitle {
+export const SurveyTitle = styled(P)`
   color: #FFA722;
   font-size: 4.44vw;
   font-weight: 700;
   line-height: 6.22vw;
   word-wrap: break-word;
-}
+`;
 
-.surveyContext{
+export const SurveyContext = styled.div`
   color: #F0F4F8;
   font-size: 2.22vw;
   font-weight: 500;
@@ -394,4 +412,4 @@ img {
   display: flex;
   gap: 1.11vw;
   flex-direction: column;
-}
+`;
