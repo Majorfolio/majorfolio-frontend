01435b26a022c5081e066c021f59d8a243572723
algoORgoal <qufstar46@gmail.com>
2024-01-11 19:48:22 +0900

    chore: replace js files with ts files

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,8 @@
-/** @type { import('@storybook/react').Preview } */
-const preview = {
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
