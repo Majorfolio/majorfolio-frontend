89558440e380a86ee67abc4a3b8a7393126fce71
algoORgoal <qufstar46@gmail.com>
2024-01-21 23:52:32 +0900

    feat: add HelperText compoennt

---
+++ src/components/common/HelperText/index.styles.tsx
@@ -0,0 +1,20 @@
+import { styled } from 'styled-components';
+
+const StyledHelperText = styled.span`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+`;
+
+export const StyledTextContainer = styled.div`
+  margin-top: 32px;
+  margin-bottom: 28px;
+`;
+
+export const StyledButtonContainer = styled.div`
+  display: flex;
+  column-gap: 14px;
+  margin-top: 40px;
+`;
+
+export default StyledHelperText;
