68641deede8a555cec849a5a258fe1dffba1c083
algoORgoal <qufstar46@gmail.com>
2024-01-26 00:35:46 +0100

    fix: change props to updated ones

--- src/stories/TextField.stories.tsx
+++ src/stories/TextField.stories.tsx
@@ -1,5 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
 import TextField from '../components/common/TextField';
+import theme from '../components/common/theme';
+import Button from '../components/common/Button';
+import Text from '../components/common/Text';
 
 const meta = {
   title: 'TextField',
@@ -10,34 +14,50 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Title: Story = {
+export const EmailConfirmed: Story = {
   args: {
-    type: 'title',
-    disabled: false,
-    active: true,
+    id: 'email',
+    type: 'email',
+    borderColor: 'gray/gray100',
+    borderColorOnHover: 'gray/gray150',
+    borderColorOnFocus: 'main_color/blue_p',
+    icon: (
+      <Button type="submit" backgroundColor="main_color/blue_p">
+        <Text color="gray/white" size={16} weight="bold" lineHeight="sm">
+          다음으로
+        </Text>
+      </Button>
+    ),
+    placeholder: '이메일',
   },
 };
 
-export const TitleDisabled: Story = {
+export const EmailInProgress: Story = {
   args: {
-    type: 'title',
-    disabled: true,
-    active: false,
-  },
-};
-
-export const Password: Story = {
-  args: {
-    type: 'password',
-    disabled: false,
-    active: true,
-  },
-};
-
-export const PasswordDisabled: Story = {
-  args: {
-    type: 'title',
-    disabled: true,
-    active: false,
+    id: 'email',
+    type: 'email',
+    borderColor: 'gray/gray100',
+    borderColorOnHover: 'gray/gray150',
+    borderColorOnFocus: 'main_color/blue_p',
+    icon: (
+      <>
+        <Button backgroundColor="sub_color/indigo/c">
+          <Text
+            color="main_color/blue_p"
+            size={16}
+            weight="bold"
+            lineHeight="sm"
+          >
+            다음에 하기
+          </Text>
+        </Button>
+        <Button backgroundColor="gray/gray100">
+          <Text color="gray/gray400" size={16} weight="bold" lineHeight="sm">
+            인증메일 전송
+          </Text>
+        </Button>
+      </>
+    ),
+    placeholder: '이메일',
   },
 };
