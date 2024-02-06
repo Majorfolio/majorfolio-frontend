70e1bc32c2f03b320b18f1c5803b8295cd0276db
algoORgoal <qufstar46@gmail.com>
2024-02-06 19:40:54 +0100

    feat: add a cursor pointer on hover

--- src/components/common/Button/index.styles.tsx
+++ src/components/common/Button/index.styles.tsx
@@ -7,9 +7,11 @@ export const StyledButton = styled.button<Required<StyledButtonPropsType>>`
   justify-content: center;
   background-color: ${(props) => theme.color[props.backgroundColor]};
   padding: 16px 0 17px 0;
-  border: ${(props) => (props.isOutlined ? `1px ${theme.color['main_color/blue_p']} solid;` : 0)};
+  border: ${(props) =>
+    props.isOutlined ? `1px ${theme.color['main_color/blue_p']} solid;` : 0};
   border-radius: 6px;
   width: 100%;
+  cursor: pointer;
 `;
 
 export default StyledButton;
