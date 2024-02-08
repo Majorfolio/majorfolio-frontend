cebd93eb4f98598922b85256adf302869a2237d0
cyLee1111 <flora021111@gmail.com>
2024-02-09 00:25:55 +0900

    refactor: change bottom bar position

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-import { CardWrapper, ContentPageContainer, HomeContainer } from './index.styles';
+import { CardWrapper, ContentPageContainer, HomeContainer, PageContainer } from './index.styles';
 import AllDivider from '../../components/common/AllDivider';
 import HomeCategoryButtonSection from '../../components/home/HomeCategoryButtonSection';
 import HomeContentPageTitle from '../../components/home/HomeContentPageTitle';
@@ -48,71 +48,73 @@ const Home = () => {
   }, []);
 
   return (
-    <HomeContainer>
-      <HomeCategoryButtonSection currentCategory={currentCategory} updateCategory={updateCategory} />
-      <AllDivider />
+    <PageContainer>
+      <HomeContainer>
+        <HomeCategoryButtonSection currentCategory={currentCategory} updateCategory={updateCategory} />
+        <AllDivider />
 
-      <ContentPageContainer>
-        <HomeContentPageTitle />
+        <ContentPageContainer>
+          <HomeContentPageTitle />
 
-        <HomeTagCardTitle title='신규 등록 자료' tag='new' />
-        <CardWrapper>
-          { homeMaterials && homeMaterials.newUpload &&
-            homeMaterials.newUpload.map((material: Material) => {
-              return (
-                <HomeMaterialCard 
-                  key={material.id}
-                  isBig={false} 
-                  id={material.id} 
-                  nickname={material.nickname} 
-                  className={material.className} 
-                  univ={material.univ ?? null} 
-                  major={material.major ?? null} 
-                  semester={material.semester ?? null} 
-                  professor={material.professor ?? null} 
-                  like={material.like ?? null} 
-                />
-              );
-            })}
-          {/* <HomeMaterialCard isBig={false} />
-          <HomeMaterialCard isBig={false} />
-          <HomeMaterialCard isBig={false} /> */}
-        </CardWrapper>
+          <HomeTagCardTitle title='신규 등록 자료' tag='new' />
+          <CardWrapper>
+            { homeMaterials && homeMaterials.newUpload &&
+              homeMaterials.newUpload.map((material: Material) => {
+                return (
+                  <HomeMaterialCard 
+                    key={material.id}
+                    isBig={false} 
+                    id={material.id} 
+                    nickname={material.nickname} 
+                    className={material.className} 
+                    univ={material.univ ?? null} 
+                    major={material.major ?? null} 
+                    semester={material.semester ?? null} 
+                    professor={material.professor ?? null} 
+                    like={material.like ?? null} 
+                  />
+                );
+              })}
+            {/* <HomeMaterialCard isBig={false} />
+            <HomeMaterialCard isBig={false} />
+            <HomeMaterialCard isBig={false} /> */}
+          </CardWrapper>
 
-        <HomeTagCardTitle title='베스트 자료' tag='hot' />
-        <CardWrapper>
-          { homeMaterials && homeMaterials.best &&
-            homeMaterials.best.map((material: Material) => {
-              return (
-                <HomeMaterialCard 
-                  key={material.id}
-                  isBig={false} 
-                  id={material.id} 
-                  nickname={material.nickname} 
-                  className={material.className} 
-                  univ={material.univ ?? null} 
-                  major={material.major ?? null} 
-                  semester={material.semester ?? null} 
-                  professor={material.professor ?? null} 
-                  like={material.like ?? null} 
-                />
-              );
-            })}
-          {/* <HomeMaterialCard isBig={false} />
-          <HomeMaterialCard isBig={false} />
-          <HomeMaterialCard isBig={false} /> */}
-        </CardWrapper>
+          <HomeTagCardTitle title='베스트 자료' tag='hot' />
+          <CardWrapper>
+            { homeMaterials && homeMaterials.best &&
+              homeMaterials.best.map((material: Material) => {
+                return (
+                  <HomeMaterialCard 
+                    key={material.id}
+                    isBig={false} 
+                    id={material.id} 
+                    nickname={material.nickname} 
+                    className={material.className} 
+                    univ={material.univ ?? null} 
+                    major={material.major ?? null} 
+                    semester={material.semester ?? null} 
+                    professor={material.professor ?? null} 
+                    like={material.like ?? null} 
+                  />
+                );
+              })}
+            {/* <HomeMaterialCard isBig={false} />
+            <HomeMaterialCard isBig={false} />
+            <HomeMaterialCard isBig={false} /> */}
+          </CardWrapper>
 
-        <HomeTagCardTitle title='최근에 본 자료' />
-        <CardWrapper>
-          {/* <HomeMaterialCard isBig={false} />
-          <HomeMaterialCard isBig={false} />
-          <HomeMaterialCard isBig={false} /> */}
-        </CardWrapper>
-      </ContentPageContainer>
+          <HomeTagCardTitle title='최근에 본 자료' />
+          <CardWrapper>
+            {/* <HomeMaterialCard isBig={false} />
+            <HomeMaterialCard isBig={false} />
+            <HomeMaterialCard isBig={false} /> */}
+          </CardWrapper>
+        </ContentPageContainer>
+      </HomeContainer>
 
       <BottomBar />
-    </HomeContainer>
+    </PageContainer>
   )
 }
 
--- src/pages/HomeViewAll/index.tsx
+++ src/pages/HomeViewAll/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-import { CardTitleWrapper, CardsWrapper, ViewAllContainer } from './index.styles'
+import { CardTitleWrapper, CardsWrapper, PageContainer, ViewAllContainer } from './index.styles'
 import HomeTagCardTitle from '../../components/home/HomeTagCardTitle'
 import HomeMaterialCard from '../../components/home/HomeMaterialCard'
 import BottomBar from '../../components/common/BottomBar'
 
 const HomeViewAll = () => {
   return (
-    <>
+    <PageContainer>
+
       <ViewAllContainer>
         <CardTitleWrapper>
           <HomeTagCardTitle title='모두보기제목' tag='hot' isViewAll />
@@ -17,10 +18,10 @@ const HomeViewAll = () => {
           <HomeMaterialCard />
           <HomeMaterialCard /> */}
         </CardsWrapper>
-
       </ViewAllContainer>
+
       <BottomBar />    
-    </>
+    </PageContainer>
   )
 }
 
