8657c5744ec6e1aded8635765d69e4099f7fd034
Chan <qufstar46@gmail.com>
2024-02-13 12:02:48 +0100

    refactor: seperate business logic

--- src/pages/Callback/index.tsx
+++ src/pages/Callback/index.tsx
@@ -1,58 +1,22 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-
-const API_URL1 =
-  'http://ec2-54-180-59-160.ap-northeast-2.compute.amazonaws.com';
-const PATH = '/member/login';
-
-const API_URL2 = 'https://majorfolio-server.shop';
+import useTokenExchange from './useTokenExchange';
+import Text from '../../components/common/Text';
 
 export default function Callback() {
-  const location = useLocation();
+  const { auth } = useTokenExchange();
+  if (!auth) {
+    return <Text>로그인 중입니다...</Text>;
+  }
 
-  useEffect(() => {
-    const queryString = location.search;
-    const searchParams = new URLSearchParams(queryString);
-    const code = searchParams.get('code');
-
-    if (code) {
-      const body = new URLSearchParams({
-        grant_type: 'authorization_code',
-        client_id: 'de13e7d19e639ae838e4735a0cf14614',
-        redirect_uri: 'http://localhost:3000/signin',
-        code,
-      }).toString();
-
-      fetch('https://kauth.kakao.com/oauth/token', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-        },
-        body,
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          const { id_token } = data;
-          console.log(id_token);
-
-          const state = sessionStorage.getItem('oauth_state');
-          const nonce = sessionStorage.getItem('openid_nonce');
-
-          return fetch(`${API_URL2}${PATH}`, {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-              authorization: `Bearer ${id_token}`,
-              state,
-              nonce,
-            },
-          });
-        })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-        });
-    }
-  }, [location]);
+  const { isMember, memberId, accessToken, refreshToken } = auth;
+  return (
+    <Text>
+      로그인에 성공했습니다! 로그인 중입니다...
+      <div>isMember{isMember}</div>
+      <div>memberId{memberId}</div>
+      <div>accessToken{accessToken}</div>
+      <div>refreshToken{refreshToken}</div>
+    </Text>
+  );
 }
