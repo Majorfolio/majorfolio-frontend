4fbd50bc77040352c4ceecb3b39ce54dc965c32b
Chan <qufstar46@gmail.com>
2024-02-15 00:48:51 +0100

    feat: add new api call

--- src/apis/member/index.ts
+++ src/apis/member/index.ts
@@ -1,9 +1,7 @@
-import { HTTP_METHODS } from '../constants';
+import { API_URL, HTTP_METHODS } from '../constants';
+import MEMBER_API_PATHS from '../constants/member';
 
-const API_URL2 = 'https://majorfolio-server.shop';
-const PATH = '/member/login';
-
-const getAuth = async (idToken: string) => {
+export const getAuth = async (idToken: string) => {
   try {
     const state = sessionStorage.getItem('oauth_state');
     const nonce = sessionStorage.getItem('openid_nonce');
@@ -16,7 +14,7 @@ const getAuth = async (idToken: string) => {
       throw new Error('에러 발생');
     }
 
-    const response = await fetch(`${API_URL2}${PATH}`, {
+    const response = await fetch(`${API_URL}${MEMBER_API_PATHS.LOGIN}`, {
       method: HTTP_METHODS.POST,
       credentials: 'include',
       headers: {
@@ -34,4 +32,67 @@ const getAuth = async (idToken: string) => {
   }
 };
 
-export default getAuth;
+export const sendCodeToEmail = async (
+  email: string,
+  accessToken: string,
+): Promise<number> => {
+  try {
+    const response = await fetch(`${API_URL}${MEMBER_API_PATHS.SCHOOL_EMAIL}`, {
+      method: HTTP_METHODS.POST,
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+      }),
+    });
+    const { result } = await response.json();
+    const { emailId } = result;
+    return emailId;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const validateCode = async (
+  emailId: number,
+  code: string,
+  accessToken: string,
+) => {
+  try {
+    const response = await fetch(`${API_URL}${MEMBER_API_PATHS.SCHOOL_EMAIL}`, {
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        emailId,
+        code,
+      }),
+    });
+    const { message } = await response.json();
+    if (message === '요청에 성공하였습니다.') {
+      return {
+        success: true,
+        message,
+      };
+    }
+    if (message === '인증 코드가 다릅니다.') {
+      return {
+        success: false,
+        message,
+      };
+    }
+    if (message === '인증 시간이 지났습니다.') {
+      return {
+        success: true,
+        message,
+      };
+    }
+    throw new Error('에러 발생');
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
