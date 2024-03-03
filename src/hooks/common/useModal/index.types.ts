970ab889cd66444efdf6794d3cd903fd1c045f0c
Chan <qufstar46@gmail.com>
2024-03-04 00:37:41 +0900

    feat: move to pages using modal

--- src/hooks/common/useModal/index.types.ts
+++ src/hooks/common/useModal/index.types.ts
@@ -1,4 +1,4 @@
 export interface CloseActionsType {
-  primaryAction: () => void;
-  secondaryAction?: () => void;
+  primaryAction: () => void | Promise<void>;
+  secondaryAction?: () => void | Promise<void>;
 }
