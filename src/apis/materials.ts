4acd4bbe32b8756d7c389c5cba54b4cdba83caa9
Chan <qufstar46@gmail.com>
2024-03-05 06:21:20 +0900

    chore: remove code comments

--- src/apis/materials.ts
+++ src/apis/materials.ts
@@ -5,106 +5,149 @@ export const getAllUniv = async () => {
 };
 
 export const getMyUniv = async (authStore: string) => {
-  // const authStore = useAuthStore((state) => state.accessToken);
   const requestOptions = {
     method: 'GET',
     headers: {
-      'Authorization': `Bearer ${authStore}`
-    }
+      Authorization: `Bearer ${authStore}`,
+    },
   };
 
-  const response = await fetch(`https://majorfolio-server.shop/home/my/univ`, requestOptions);
+  const response = await fetch(
+    `https://majorfolio-server.shop/home/my/univ`,
+    requestOptions,
+  );
   const data = await response.json();
   return data;
 };
 
 export const getMyMajor = async (authStore: string) => {
-  // const authStore = useAuthStore((state) => state.accessToken);
   const requestOptions = {
     method: 'GET',
     headers: {
-      'Authorization': `Bearer ${authStore}`
-    }
+      Authorization: `Bearer ${authStore}`,
+    },
   };
 
-  const response = await fetch(`https://majorfolio-server.shop/home/my/major`, requestOptions);
+  const response = await fetch(
+    `https://majorfolio-server.shop/home/my/major`,
+    requestOptions,
+  );
   const data = await response.json();
   return data;
 };
 
-export const getAllUnivNewlyViewAll = async (page: number, pageSize: number) => {
-  const response = await fetch(`https://majorfolio-server.shop/home/all/univ/newly-upload?page=${page}&pageSize=${pageSize}`);
+export const getAllUnivNewlyViewAll = async (
+  page: number,
+  pageSize: number,
+) => {
+  const response = await fetch(
+    `https://majorfolio-server.shop/home/all/univ/newly-upload?page=${page}&pageSize=${pageSize}`,
+  );
   const data = await response.json();
   return data;
 };
 
 export const getAllUnivBestViewAll = async (page: number, pageSize: number) => {
-  const response = await fetch(`https://majorfolio-server.shop/home/all/univ/likes?page=${page}&pageSize=${pageSize}`);
+  const response = await fetch(
+    `https://majorfolio-server.shop/home/all/univ/likes?page=${page}&pageSize=${pageSize}`,
+  );
   const data = await response.json();
   return data;
 };
 
-export const getMyUnivNewlyViewAll = async (page: number, pageSize: number, authStore: string) => {
-  // const authStore = useAuthStore((state) => state.accessToken);
+export const getMyUnivNewlyViewAll = async (
+  page: number,
+  pageSize: number,
+  authStore: string,
+) => {
   const requestOptions = {
     method: 'GET',
     headers: {
-      'Authorization': `Bearer ${authStore}`
-    }
+      Authorization: `Bearer ${authStore}`,
+    },
   };
-  
-  const response = await fetch(`https://majorfolio-server.shop/home/my/univ/newly-upload?page=${page}&pageSize=${pageSize}`, requestOptions);
+
+  const response = await fetch(
+    `https://majorfolio-server.shop/home/my/univ/newly-upload?page=${page}&pageSize=${pageSize}`,
+    requestOptions,
+  );
   const data = await response.json();
   return data;
 };
 
-export const getMyUnivBestViewAll = async (page: number, pageSize: number, authStore: string) => {
-  // const authStore = useAuthStore((state) => state.accessToken);
+export const getMyUnivBestViewAll = async (
+  page: number,
+  pageSize: number,
+  authStore: string,
+) => {
   const requestOptions = {
     method: 'GET',
     headers: {
-      'Authorization': `Bearer ${authStore}`
-    }
+      Authorization: `Bearer ${authStore}`,
+    },
   };
-  
-  const response = await fetch(`https://majorfolio-server.shop/home/my/univ/likes?page=${page}&pageSize=${pageSize}`, requestOptions);
+
+  const response = await fetch(
+    `https://majorfolio-server.shop/home/my/univ/likes?page=${page}&pageSize=${pageSize}`,
+    requestOptions,
+  );
   const data = await response.json();
   return data;
 };
 
-export const getMyMajorNewlyViewAll = async (page: number, pageSize: number, authStore: string) => {
-  // const authStore = useAuthStore((state) => state.accessToken);
+export const getMyMajorNewlyViewAll = async (
+  page: number,
+  pageSize: number,
+  authStore: string,
+) => {
   const requestOptions = {
     method: 'GET',
     headers: {
-      'Authorization': `Bearer ${authStore}`
-    }
+      Authorization: `Bearer ${authStore}`,
+    },
   };
-  
-  const response = await fetch(`https://majorfolio-server.shop/home/my/major/newly-upload?page=${page}&pageSize=${pageSize}`, requestOptions);
+
+  const response = await fetch(
+    `https://majorfolio-server.shop/home/my/major/newly-upload?page=${page}&pageSize=${pageSize}`,
+    requestOptions,
+  );
   const data = await response.json();
   return data;
 };
 
-export const getMyMajorBestViewAll = async (page: number, pageSize: number, authStore: string) => {
-  // const authStore = useAuthStore((state) => state.accessToken);
+export const getMyMajorBestViewAll = async (
+  page: number,
+  pageSize: number,
+  authStore: string,
+) => {
   const requestOptions = {
     method: 'GET',
     headers: {
-      'Authorization': `Bearer ${authStore}`
-    }
+      Authorization: `Bearer ${authStore}`,
+    },
   };
-  
-  const response = await fetch(`https://majorfolio-server.shop/home/my/major/likes?page=${page}&pageSize=${pageSize}`, requestOptions);
+
+  const response = await fetch(
+    `https://majorfolio-server.shop/home/my/major/likes?page=${page}&pageSize=${pageSize}`,
+    requestOptions,
+  );
   const data = await response.json();
   return data;
 };
 
-export const getMaterialDetail = async (materialId: number, authStore?: string) => {
-  const headers: HeadersInit = authStore ? { Authorization: `Bearer ${authStore}` } : {};
-  const response = await fetch(`https://majorfolio-server.shop/assignment/${materialId}/detail`, {
-    headers
-  });
+export const getMaterialDetail = async (
+  materialId: number,
+  authStore?: string,
+) => {
+  const headers: HeadersInit = authStore
+    ? { Authorization: `Bearer ${authStore}` }
+    : {};
+  const response = await fetch(
+    `https://majorfolio-server.shop/assignment/${materialId}/detail`,
+    {
+      headers,
+    },
+  );
   const data = await response.json();
   return data;
 };
@@ -112,35 +155,37 @@ export const getMaterialDetail = async (materialId: number, authStore?: string) 
 export const updateLike = async (materialId: number, authStore: string) => {
   try {
     const requestOptions = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${authStore}`
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authStore}`,
       },
       body: JSON.stringify({}),
-    }
-    return await fetch(`https://majorfolio-server.shop/my/${materialId}/like`, requestOptions);    
-  } catch (e) { 
-    // console.log('updateLike error');
+    };
+    return await fetch(
+      `https://majorfolio-server.shop/my/${materialId}/like`,
+      requestOptions,
+    );
+  } catch (e) {
     return Promise.reject(e);
-  };
-
+  }
 };
 
 export const updateBookmark = async (materialId: number, authStore: string) => {
   try {
     const requestOptions = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${authStore}`
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authStore}`,
       },
       body: JSON.stringify({}),
-    }
-    return await fetch(`https://majorfolio-server.shop/my/${materialId}/bookmark`, requestOptions);    
-  } catch (e) { 
-    // console.log('updateLike error');
+    };
+    return await fetch(
+      `https://majorfolio-server.shop/my/${materialId}/bookmark`,
+      requestOptions,
+    );
+  } catch (e) {
     return Promise.reject(e);
-  };
-
+  }
 };
