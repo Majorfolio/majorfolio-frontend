b54cf5b9a62b6120cac62b6d3b5d627a73fbc77f
Chan <qufstar46@gmail.com>
2024-03-06 04:43:01 +0900

    feat: create Transactions page boilerplate

--- src/pages/My/Transactions/index.tsx
+++ src/pages/My/Transactions/index.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { SecondaryTopbar } from '../../../components/common/TopBar';
+import { ArrowBackDefaultIcon } from '../../../assets/icons';
+import Text from '../../../components/common/Text';
+import TapMenu, { SelectableTapMenu } from '../../../components/common/TapMenu';
 
 export default function Transactions() {
-  return <div>Transactions</div>;
+  const [selectedTab, setSelectedTab] = useState<number>(0);
+
+  const topBar = (
+    <SecondaryTopbar
+      transition={
+        <button type="button" onClick={() => {}} aria-label="prev">
+          <ArrowBackDefaultIcon />
+        </button>
+      }
+      title={
+        <Text color="gray/gray900" size={18} weight="bold" lineHeight="sm">
+          거래 내역
+        </Text>
+      }
+    />
+  );
+
+  const tabBar = (
+    <SelectableTapMenu
+      labels={['구매 내역', '판매 내역']}
+      selectedItem={selectedTab}
+      onItemClick={(index) => setSelectedTab(index)}
+    />
+  );
+
+  return (
+    <>
+      {topBar}
+      {tabBar}
+    </>
+  );
 }
