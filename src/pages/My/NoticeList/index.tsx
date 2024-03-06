87d4d47172e41f64b29c04f45375cb5d30ac2328
Chan <qufstar46@gmail.com>
2024-03-06 10:54:29 +0900

    feat: create NoticeList page

--- src/pages/My/NoticeList/index.tsx
+++ src/pages/My/NoticeList/index.tsx
@@ -1,5 +1,71 @@
 import React from 'react';
+import { StyledSectionContainer } from '../../../components/common/UploadSection/index.styles';
+import Description from '../../../components/common/Description';
+import RowButton from '../../../components/common/RowButton';
+import { LargeTag } from '../../../components/common/Tag';
+import { SecondaryTopbar } from '../../../components/common/TopBar';
+import { CloseDefaultIcon } from '../../../assets/icons';
+import Text from '../../../components/common/Text';
+import StyledPageContainer from '../../Upload/UploadDefaultStep/index.styles';
 
 export default function NoticeList() {
-  return <div>NoticeList</div>;
+  const description = (
+    <Description
+      normalText="메이저폴리오의"
+      boldText="새로운 소식을 확인해보세요"
+    />
+  );
+
+  const noticeList = (
+    <>
+      <RowButton
+        tag={
+          <LargeTag
+            backgroundColor="gray/gray100"
+            size={12}
+            weight="bold"
+            lineHeight="sm"
+            color="gray/gray900"
+          >
+            공지
+          </LargeTag>
+        }
+        text="개인정보처리방침 개정 안내"
+      />
+      <RowButton
+        tag={
+          <LargeTag
+            backgroundColor="sub_color/yellow/c"
+            size={12}
+            weight="bold"
+            lineHeight="sm"
+            color="main_color/yellow_s"
+          >
+            혜택
+          </LargeTag>
+        }
+        text="사전예약 시 혜택을 살펴보세요"
+      />
+    </>
+  );
+  return (
+    <>
+      <SecondaryTopbar
+        transition={
+          <button type="button" onClick={() => {}} aria-label="previous">
+            <CloseDefaultIcon />
+          </button>
+        }
+        title={
+          <Text size={18} weight="bold" lineHeight="sm" color="gray/gray900">
+            공지사항
+          </Text>
+        }
+      />
+      <StyledPageContainer>
+        {description}
+        <StyledSectionContainer>{noticeList}</StyledSectionContainer>
+      </StyledPageContainer>
+    </>
+  );
 }
