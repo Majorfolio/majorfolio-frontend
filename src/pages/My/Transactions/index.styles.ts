418657a9314498d5da88757bce95b3d59cec48d3
Chan <qufstar46@gmail.com>
2024-03-07 13:44:56 +0900

    fix: use styled-component for derived component

--- src/pages/My/Transactions/index.styles.ts
+++ src/pages/My/Transactions/index.styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 import Card from '../../../components/common/Card';
-import Column from '../../../components/common/Column';
+import StyledColumn from '../../../components/common/Column/index.styles';
 
-const StyledTransactionCardContainer = styled(Column)`
+const StyledTransactionCardContainer = styled(StyledColumn)`
   box-shadow: 0px 0px 16px 0px rgba(35, 38, 41, 0.04);
   box-sizing: border-box;
 `;
