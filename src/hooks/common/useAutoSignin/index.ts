113118ed33325e555ae594d7fd428266bf9df61f
Chan <qufstar46@gmail.com>
2024-03-08 20:01:32 +0900

    fix: add setState function to dependency array of useEffect

--- src/hooks/common/useAutoSignin/index.ts
+++ src/hooks/common/useAutoSignin/index.ts
@@ -10,7 +10,7 @@ export default function useAutoSignin() {
   useEffect(() => {
     restoreCredentials();
     setIsSigninDone(true);
-  }, [restoreCredentials]);
+  }, [restoreCredentials, setIsSigninDone]);
 
   return { isSigninDone };
 }
