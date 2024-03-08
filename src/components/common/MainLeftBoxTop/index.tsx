408232674caf5c664d724c58856e50ca275dca99
Chan <qufstar46@gmail.com>
2024-03-08 11:23:01 +0900

    fix: remove login text when user is logged in

--- src/components/common/MainLeftBoxTop/index.tsx
+++ src/components/common/MainLeftBoxTop/index.tsx
@@ -1,36 +1,56 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { BottomWrapper, DescriptionMainTextWrapper, LeftBoxTopContainer, LoginButton, MiddleDescriptionWrapper, TopLogoWrapper } from './index.styles'
-import { Logo1 } from '../../../assets/images'
-import Text from '../Text'
+import {
+  BottomWrapper,
+  DescriptionMainTextWrapper,
+  LeftBoxTopContainer,
+  LoginButton,
+  MiddleDescriptionWrapper,
+  TopLogoWrapper,
+} from './index.styles';
+import { Logo1 } from '../../../assets/images';
+import Text from '../Text';
+import useAuthStore, { AuthLevel } from '../../../store/useAuthStore';
 
 const MainLeftBoxTop = () => {
   const navigate = useNavigate();
+  const authLevel = useAuthStore((state) => state.authLevel);
+
   const handleLoginButtonClick = () => {
     navigate('/signin');
-  }
+  };
 
   return (
     <LeftBoxTopContainer>
       <TopLogoWrapper>
         <Logo1 />
-        <LoginButton onClick={handleLoginButtonClick}>
-          <Text size={18} lineHeight='lg'>로그인</Text>
-        </LoginButton>
+        {authLevel === AuthLevel.Guest && (
+          <LoginButton onClick={handleLoginButtonClick}>
+            <Text size={18} lineHeight="lg">
+              로그인
+            </Text>
+          </LoginButton>
+        )}
       </TopLogoWrapper>
 
       <MiddleDescriptionWrapper>
         <DescriptionMainTextWrapper>
-          <Text size={52} lineHeight='lg'>시성비 최대로!</Text>
-          <Text size={52} weight='bold' lineHeight='lg'>우리만의 과제장터</Text>        
+          <Text size={52} lineHeight="lg">
+            시성비 최대로!
+          </Text>
+          <Text size={52} weight="bold" lineHeight="lg">
+            우리만의 과제장터
+          </Text>
         </DescriptionMainTextWrapper>
-        <Text size={22} lineHeight='lg'>커피 한 잔에 나누는 A+ 비법, <br /> 과제 시간을 반으로 줄여보세요!</Text>
+        <Text size={22} lineHeight="lg">
+          커피 한 잔에 나누는 A+ 비법, <br /> 과제 시간을 반으로 줄여보세요!
+        </Text>
       </MiddleDescriptionWrapper>
 
       <BottomWrapper />
     </LeftBoxTopContainer>
-  )
-}
+  );
+};
 
-export default MainLeftBoxTop
+export default MainLeftBoxTop;
