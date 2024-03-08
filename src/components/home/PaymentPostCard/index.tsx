443376414b2059ba3c9644aa411113f2de7f1dde
Chan <qufstar46@gmail.com>
2024-03-08 11:55:24 +0900

    chore: remove unnecessary imports

--- src/components/home/PaymentPostCard/index.tsx
+++ src/components/home/PaymentPostCard/index.tsx
@@ -1,13 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react';
 
-import { CardTitleWrapper, CardWrapper, InfoWrapper, InfosWrapper, MaterialPriceWrapper } from './index.styles';
+import {
+  CardTitleWrapper,
+  CardWrapper,
+  InfoWrapper,
+  InfosWrapper,
+  MaterialPriceWrapper,
+} from './index.styles';
 import Text from '../../common/Text';
 import AllCheckbox from '../../common/AllCheckbox';
-import { 
-  SchoolDefaultIcon, 
-  DepartmentOutlinedIcon, 
-  SemesterDefaultIcon, 
-  ClassDefaultIcon 
+import {
+  SchoolDefaultIcon,
+  DepartmentOutlinedIcon,
+  SemesterDefaultIcon,
+  ClassDefaultIcon,
 } from '../../../assets/icons';
 import { MaterialDetail } from '../Material/index.types';
 
@@ -17,46 +23,60 @@ interface PaymentPostCardProps {
 }
 
 function PaymentPostCard({ isCart, materialInfo }: PaymentPostCardProps) {
-  return (
-    materialInfo ?
-      <CardWrapper>
-        <CardTitleWrapper>
-          <Text size={20} weight='bold' lineHeight='sm' color='gray/gray900'> {materialInfo.title} </Text>
-          { isCart ?
-            <AllCheckbox checked={false} /> :
-            null
-          }
-        </CardTitleWrapper>
-        
-        <InfosWrapper>
-          <InfoWrapper>
-            <SchoolDefaultIcon />
-            <Text size={14} color='gray/gray500'> {materialInfo.university} </Text>
-          </InfoWrapper>
+  return materialInfo ? (
+    <CardWrapper>
+      <CardTitleWrapper>
+        <Text size={20} weight="bold" lineHeight="sm" color="gray/gray900">
+          {' '}
+          {materialInfo.title}{' '}
+        </Text>
+        {isCart ? <AllCheckbox checked={false} /> : null}
+      </CardTitleWrapper>
 
-          <InfoWrapper>
-            <DepartmentOutlinedIcon />
-            <Text size={14} color='gray/gray500'> {materialInfo.major} </Text>
-          </InfoWrapper>
+      <InfosWrapper>
+        <InfoWrapper>
+          <SchoolDefaultIcon />
+          <Text size={14} color="gray/gray500">
+            {' '}
+            {materialInfo.university}{' '}
+          </Text>
+        </InfoWrapper>
 
-          <InfoWrapper>
-            <SemesterDefaultIcon />
-            <Text size={14} color='gray/gray500'> {materialInfo.semester} </Text>
-          </InfoWrapper>
+        <InfoWrapper>
+          <DepartmentOutlinedIcon />
+          <Text size={14} color="gray/gray500">
+            {' '}
+            {materialInfo.major}{' '}
+          </Text>
+        </InfoWrapper>
 
-          <InfoWrapper>
-            <ClassDefaultIcon />
-            <Text size={14} color='gray/gray500'> {materialInfo.subjectTitle} </Text>
-          </InfoWrapper>
-        </InfosWrapper>
+        <InfoWrapper>
+          <SemesterDefaultIcon />
+          <Text size={14} color="gray/gray500">
+            {' '}
+            {materialInfo.semester}{' '}
+          </Text>
+        </InfoWrapper>
 
-        <MaterialPriceWrapper>
-          <Text size={14} color='gray/gray500'>자료 금액</Text>
-          <Text weight='bold' color='gray/gray900'>4,700원</Text>
-        </MaterialPriceWrapper>
-      </CardWrapper>
-      : null
-  )
+        <InfoWrapper>
+          <ClassDefaultIcon />
+          <Text size={14} color="gray/gray500">
+            {' '}
+            {materialInfo.subjectTitle}{' '}
+          </Text>
+        </InfoWrapper>
+      </InfosWrapper>
+
+      <MaterialPriceWrapper>
+        <Text size={14} color="gray/gray500">
+          자료 금액
+        </Text>
+        <Text weight="bold" color="gray/gray900">
+          4,700원
+        </Text>
+      </MaterialPriceWrapper>
+    </CardWrapper>
+  ) : null;
 }
 
-export default PaymentPostCard
+export default PaymentPostCard;
--- src/pages/Callback/index.tsx
+++ src/pages/Callback/index.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import React from 'react';
 import useTokenExchange from './useTokenExchange';
 import Text from '../../components/common/Text';
 
