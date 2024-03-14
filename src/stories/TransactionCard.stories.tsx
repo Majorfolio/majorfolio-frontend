95af4854d68c41b440e76ecc6d63d8aa64c7cb92
Chan <qufstar46@gmail.com>
2024-03-14 13:41:12 +0900

    fix: props error in storybook

--- src/stories/TransactionCard.stories.tsx
+++ src/stories/TransactionCard.stories.tsx
@@ -1,17 +1,26 @@
-// import React from 'react';
-// import { Meta, StoryObj } from '@storybook/react';
-// import Transactions, { TransactionCard } from '../pages/My/Transactions';
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import Transactions, { TransactionCard } from '../pages/My/Transactions';
 
-// const meta = {
-//   title: 'My/Transaction/TransactionCard',
-//   component: TransactionCard,
-// } satisfies Meta;
+const meta = {
+  title: 'My/Transaction/TransactionCard',
+  component: TransactionCard,
+} satisfies Meta;
 
-// export default meta;
+export default meta;
 
-// type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof meta>;
 
-// export const Default: Story = {};
-// export default {};
-// eslint-disable-next-line storybook/story-exports
-export default {};
+export const Default: Story = {
+  args: {
+    category: 'beforePay',
+    material: {
+      id: 123,
+      className: 'C프로그래밍',
+      univ: '건국대학교',
+      major: '컴퓨터공학부',
+      price: 4700,
+      updateAt: '2023-01-01',
+    },
+  },
+};
