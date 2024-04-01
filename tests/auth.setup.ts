88def3f2ced429c43c1c56dce5cd1d786575dc35
Chan <qufstar46@gmail.com>
2024-04-01 23:21:16 +0900

    feat: setup playwright

---
+++ tests/auth.setup.ts
@@ -0,0 +1,23 @@
+import { test as setup } from './test';
+
+const authFile = 'playwright/.auth/user.json';
+
+setup('authenticate', async ({ page }) => {
+  await page.goto('/signin');
+
+  await page.getByRole('button', { name: /카카오/ }).click();
+  await page.waitForURL(/https:\/\/accounts.kakao.com\/login/);
+  await page
+    .getByRole('textbox', { name: /account information|메일/i })
+    .fill(`${process.env.SIGNIN_VIA_KAKAO_USERNAME}`);
+  await page
+    .getByRole('textbox', { name: /password|비밀번호/i })
+    .fill(`${process.env.SIGNIN_VIA_KAKAO_PASSWORD}`);
+  await page.getByRole('button', { name: /^(log in|로그인)$/i }).click();
+
+  await page.waitForURL(/https:\/\/logins.daum.net/);
+  await page.reload();
+  await page.waitForURL('http://localhost:3000/home');
+
+  await page.context().storageState({ path: authFile });
+});
