107a3e0c3691dfb79355ef4bf1eec8bcb47b08c0
Chan <qufstar46@gmail.com>
2024-04-03 16:20:06 +0900

    test: mock authentication in playwright

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -5,22 +5,27 @@ const authFile = 'playwright/.auth/user.json';
 setup('authenticate', async ({ page }) => {
   await page.goto('/signin');
   await page.getByRole('button', { name: /카카오/ }).click();
-  await page.waitForURL(/https:\/\/accounts.kakao.com\/login/);
-  await expect(
-    page.getByRole('textbox', { name: /account information|메일/i }),
-  ).toBeVisible();
-  await page
-    .getByRole('textbox', { name: /account information|메일/i })
-    .fill(`${process.env.SIGNIN_VIA_KAKAO_USERNAME}`);
-  await page
-    .getByRole('textbox', { name: /password|비밀번호/i })
-    .fill(`${process.env.SIGNIN_VIA_KAKAO_PASSWORD}`);
-  await page.getByRole('button', { name: /^(log in|로그인)$/i }).click();
 
-  await page.waitForLoadState('domcontentloaded');
-  await page.waitForURL(/https:\/\/logins.daum.net/);
-  await page.reload();
-  await page.waitForURL('http://localhost:3000/home');
+  // real-world application workflow
+  // await page.waitForURL(/https:\/\/accounts.kakao.com\/login/);
+  // await expect(
+  //   page.getByRole('textbox', { name: /account information|메일/i }),
+  // ).toBeVisible();
+  // await page
+  //   .getByRole('textbox', { name: /account information|메일/i })
+  //   .fill(`${process.env.SIGNIN_VIA_KAKAO_USERNAME}`);
+  // await page
+  //   .getByRole('textbox', { name: /password|비밀번호/i })
+  //   .fill(`${process.env.SIGNIN_VIA_KAKAO_PASSWORD}`);
+  // await page.getByRole('button', { name: /^(log in|로그인)$/i }).click();
+
+  // await page.waitForURL(/https:\/\/logins.daum.net/);
+  // await page.reload();
+
+  // await page.waitForURL('http://localhost:3000/home');
+
+  await page.waitForURL(/\/callback/);
+  await page.waitForURL(/\/home/);
 
   await page.context().storageState({ path: authFile });
 });
